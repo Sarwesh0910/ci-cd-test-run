bfc10d8906a01137ed32b71fd36a739ffa26273e
Sarwesh0910 <sarweshudhaya@gmail.com>
2025-09-27 19:37:26 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,8 @@
 from app import app
 
 def test_health():
-    response = app.test_client().get('/health')
+    app.testing = True
+    client = app.test_client()
+    response = client.get('/health')
     assert response.status_code == 200
-    assert response.data == b"OK"
+    assert response.data == b'OK'
